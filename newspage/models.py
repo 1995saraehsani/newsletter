cb8d52c812c6e18637137192ae254bd18c77f6d4
sara ehsani <sara.ehsani.ardestani@gmail.com>
2022-06-06 18:15:02 +0430

    home page html changed

--- newspage/models.py
+++ newspage/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from datetime import date
 from django.urls import reverse
+from django.conf import settings
 
 # Create your models here.
 class AuthorSportPage(models.Model):
@@ -19,7 +20,7 @@ class AuthorSportPage(models.Model):
     def __str__(self):
         return self.text
     def get_absolute_url(self):
-        return reverse("sport_detail", args=[str(self.id)])
+        return reverse("sport_detail", args=[str(self.id)])  
     
     
 class AuthorTechnologyPage(models.Model):
